a384505908856863d2195d588a5e5f8095164e5f
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2021-03-21 13:16:30 -0300

    Challenge Completed

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const emailAlreadyExist = this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyExist) {
+      throw new Error("Email already exists!");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
   }
 }
 
